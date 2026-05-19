a7c507341fd1e04efd7623813bd6f76b334d3488
rnair1-ch <rnair1@companieshouse.gov.uk>
2026-05-19 19:26:48 +0100

    TRACS-26- Set up initial Spring Boot service skeleton

---
+++ src/main/java/uk/gov/companieshouse/strike_off_partner_objections_api/StrikeOffPartnerObjectionsApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.strike_off_partner_objections_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrikeOffPartnerObjectionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrikeOffPartnerObjectionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/strike_off_partner_objections_api/StrikeOffPartnerObjectionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.strike_off_partner_objections_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrikeOffPartnerObjectionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
