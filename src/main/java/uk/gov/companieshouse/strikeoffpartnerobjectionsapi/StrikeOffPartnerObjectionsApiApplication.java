d52df896ba516a96ef02d58b86bff0f5ef96b92c
ppeasegood1-ch <ppeasegood1@companieshouse.gov.uk>
2026-05-22 15:53:25 +0100

    TRACs-37 added logging statement

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplication.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplication.java
@@ -1,12 +1,18 @@
 package uk.gov.companieshouse.strikeoffpartnerobjectionsapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StrikeOffPartnerObjectionsApiApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+            StrikeOffPartnerObjectionsApiApplication.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting strike-off-partner-objections-api application");
         SpringApplication.run(StrikeOffPartnerObjectionsApiApplication.class, args);
     }
 
