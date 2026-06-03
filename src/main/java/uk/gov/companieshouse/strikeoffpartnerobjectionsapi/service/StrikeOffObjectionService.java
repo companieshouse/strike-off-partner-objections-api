fc7588a8106f526c83dd44265cdc82615ebbf56b
ppeasegood1-ch <ppeasegood1@companieshouse.gov.uk>
2026-06-03 16:14:37 +0100

    sonarcube fix

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/service/StrikeOffObjectionService.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/service/StrikeOffObjectionService.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.strikeoffpartnerobjectionsapi.service;
 import java.time.Instant;
 import java.util.UUID;
 import java.util.Map;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,13 @@ public class StrikeOffObjectionService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StrikeOffObjectionService.class);
     private static final String INITIAL_PROCESSING_STATUS = "PENDING";
+    private static final Pattern CONTROL_CHARS = Pattern.compile("[\\r\\n\\t\\f\\u0000-\\u007F]");
 
     public BaseObjectionResponse createObjection(final String companyNumber,
                                                  final CreateObjectionRequest createObjectionRequest) {
-        LOGGER.info("Creating objection for company number {}",
-                companyNumber);
+        LOGGER.info("Creating objection for company number {} and partner reference {}",
+                sanitizeForLog(companyNumber),
+                sanitizeForLog(createObjectionRequest.getPartnerCaseReference()));
 
         final String objectionId = UUID.randomUUID().toString();
         final String selfLink = String.format(
@@ -32,6 +35,13 @@ public class StrikeOffObjectionService {
                 Map.of("self", selfLink),
                 Instant.now(),
                 UUID.randomUUID().toString());
+    }
+
+    private String sanitizeForLog(final String value) {
+        if (value == null) {
+            return "null";
+        }
+        return CONTROL_CHARS.matcher(value).replaceAll("_");
     }
 }
 
