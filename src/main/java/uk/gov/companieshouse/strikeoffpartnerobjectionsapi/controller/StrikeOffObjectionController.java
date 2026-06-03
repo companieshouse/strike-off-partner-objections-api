38e11c3f5178f761757f31944e5102cd39007b29
ppeasegood1-ch <ppeasegood1@companieshouse.gov.uk>
2026-06-03 12:35:56 +0100

    create working createObjection endpoint

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/controller/StrikeOffObjectionController.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/controller/StrikeOffObjectionController.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/strike-off-partner-objections-api")
 @RestController
 public class StrikeOffObjectionController {
 
