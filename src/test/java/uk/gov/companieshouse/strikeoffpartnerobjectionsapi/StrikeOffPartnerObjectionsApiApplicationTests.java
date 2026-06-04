1c8d87887e2648a5e1298da995118ddf31414a40
sampankumar <skumar@companieshouse.gov.uk>
2026-06-04 12:28:11 +0100

    Adding testcontainers for mongo for docker testing of mongo url

--- src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplicationTests.java
@@ -7,8 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Objects;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,12 +24,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         }
 )
 @AutoConfigureMockMvc
+@Testcontainers
 class StrikeOffPartnerObjectionsApiApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7");
 
     @DynamicPropertySource
     static void mongoProperties(DynamicPropertyRegistry registry) {
-        String mongoUrl = System.getenv("MONGODB_URL");
-        registry.add("spring.mongodb.uri", () -> Objects.requireNonNullElse(mongoUrl, "mongodb://localhost:27017/strike_off_partner_objections"));
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
 
     @Autowired
