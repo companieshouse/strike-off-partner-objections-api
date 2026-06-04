85f3e86531db691d88f70c020ef1b8c732558b64
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-06-04 10:29:58 +0100

    TRACS-34 - unit tests modified and dependencies removed

--- src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplicationTests.java
@@ -4,12 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,16 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         }
 )
 @AutoConfigureMockMvc
-@Testcontainers
 class StrikeOffPartnerObjectionsApiApplicationTests {
-
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7");
-
-    @DynamicPropertySource
-    static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-    }
 
     @Autowired
     private MockMvc mockMvc;
