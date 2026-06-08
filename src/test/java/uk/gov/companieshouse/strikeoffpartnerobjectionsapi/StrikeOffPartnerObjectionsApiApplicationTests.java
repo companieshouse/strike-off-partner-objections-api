d2b53cef1578d6bacdb328efd5928d87b3445f0b
ppeasegood1-ch <ppeasegood1@companieshouse.gov.uk>
2026-06-08 13:53:44 +0100

    TRACS-45 fix merge issue

--- src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/StrikeOffPartnerObjectionsApiApplicationTests.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.strikeoffpartnerobjectionsapi;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
