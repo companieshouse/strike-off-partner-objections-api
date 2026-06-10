67da29dc02a3e9e46c308e201d4ac7f86d9886d2
schalla-ch <schalla@companieshouse.gov.uk>
2026-06-10 17:08:15 +0100

    TRACS-49 Use OBJECTION KIND from api spec enum

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/mapper/ObjectionRequestMapper.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/mapper/ObjectionRequestMapper.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.strikeoffpartnerobjectionsapi.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import uk.gov.companieshouse.api.objections.model.CallbackResourceKind;
 import uk.gov.companieshouse.api.objections.model.CreateObjectionRequest;
 import uk.gov.companieshouse.api.objections.model.PartnerObjectionReason;
 import uk.gov.companieshouse.api.objections.model.PartnerObjectionWorkstream;
@@ -10,7 +11,6 @@ import uk.gov.companieshouse.strikeoffpartnerobjectionsapi.model.ObjectionDocume
 import uk.gov.companieshouse.strikeoffpartnerobjectionsapi.model.ObjectionLinks;
 
 import static uk.gov.companieshouse.api.objections.model.ObjectionProcessingStatus.OBJECTION_SUBMITTED;
-import static uk.gov.companieshouse.strikeoffpartnerobjectionsapi.utils.StrikeoffPartnerObjectionsUtils.OBJECTION_KIND;
 
 @Mapper(componentModel = "spring")
 public interface ObjectionRequestMapper {
@@ -18,7 +18,7 @@ public interface ObjectionRequestMapper {
     @Mapping(target = "objectionId", source = "objectionId")
     @Mapping(target = "processingStatus", expression = "java(getStatus())")
     @Mapping(target = "etag", source = "etag")
-    @Mapping(target = "kind", constant = OBJECTION_KIND)
+    @Mapping(target = "kind", expression = "java(getObjectionKind())")
     @Mapping(target = "links", expression = "java(buildLinks(companyNumber, objectionId))")
     @Mapping(target = "companyNumber", source = "companyNumber")
     @Mapping(target = "partnerOrganisation", source = "partnerOrganisation")
@@ -44,11 +44,18 @@ public interface ObjectionRequestMapper {
         return value == null ? null : value.getValue();
     }
 
+    /**
+     * KIND is constant for all objections created by this API, so it is set as a constant in the mapping annotation.
+     */
+    default String getObjectionKind() {
+        return CallbackResourceKind.STRIKE_OFF_PARTNER_OBJECTION_OBJECTION.getValue();
+    }
+
+
     @Named("reasonToString")
     default String reasonToString(PartnerObjectionReason value) {
         return value == null ? null : value.getValue();
     }
-
 
     default ObjectionLinks buildLinks(String companyNumber, String objectionId) {
         ObjectionLinks links = new ObjectionLinks();
--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/utils/StrikeoffPartnerObjectionsUtils.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/utils/StrikeoffPartnerObjectionsUtils.java
@@ -8,7 +8,6 @@ public class StrikeoffPartnerObjectionsUtils {
         /* This utility class should not be instantiated */
     }
 
-    public static final String OBJECTION_KIND = "strike-off-partner-objection#objection";
     public static final String APPLICATION_NAMESPACE = "strike-off-partner-objections-api";
     public static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAMESPACE);
 }
