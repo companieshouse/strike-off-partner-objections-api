32166641f75d0d14ce6828f2ad5c9dadbd968809
schalla-ch <schalla@companieshouse.gov.uk>
2026-06-15 11:13:08 +0100

    TRACS- Using lombok getter/setter

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/model/ObjectionDocument.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/model/ObjectionDocument.java
@@ -2,19 +2,24 @@ package uk.gov.companieshouse.strikeoffpartnerobjectionsapi.model;
 
 import java.time.Instant;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@Getter
+@Setter
 @Document(collection = "objections")
 public class ObjectionDocument {
 
 	@Id
 	private String id;
 
-	@Field("company_number")
+	@Setter
+    @Field("company_number")
 	private String companyNumber;
 
 	@Field("partner_organisation")
@@ -60,133 +65,5 @@ public class ObjectionDocument {
 	private ObjectionLinks links;
 
 	private String kind;
-
-	public String getId() {
-		return id;
-	}
-
-	public String getCompanyNumber() {
-		return companyNumber;
-	}
-
-	public void setCompanyNumber(String companyNumber) {
-		this.companyNumber = companyNumber;
-	}
-
-	public String getSubmissionCompanyName() {
-		return submissionCompanyName;
-	}
-
-	public void setSubmissionCompanyName(String submissionCompanyName) {
-		this.submissionCompanyName = submissionCompanyName;
-	}
-
-	public String getPartnerCaseReference() {
-		return partnerCaseReference;
-	}
-
-	public void setPartnerCaseReference(String partnerCaseReference) {
-		this.partnerCaseReference = partnerCaseReference;
-	}
-
-	public String getPartnerObjectionWorkstream() {
-		return partnerObjectionWorkstream;
-	}
-
-	public void setPartnerObjectionWorkstream(String partnerObjectionWorkstream) {
-		this.partnerObjectionWorkstream = partnerObjectionWorkstream;
-	}
-
-	public String getPartnerObjectionReason() {
-		return partnerObjectionReason;
-	}
-
-	public void setPartnerObjectionReason(String partnerObjectionReason) {
-		this.partnerObjectionReason = partnerObjectionReason;
-	}
-
-	public String getPartnerContactEmail() {
-		return partnerContactEmail;
-	}
-
-	public void setPartnerContactEmail(String partnerContactEmail) {
-		this.partnerContactEmail = partnerContactEmail;
-	}
-
-	public String getObjectionId() {
-		return objectionId;
-	}
-
-	public void setObjectionId(String objectionId) {
-		this.objectionId = objectionId;
-	}
-
-	public String getProcessingStatus() {
-		return processingStatus;
-	}
-
-	public void setProcessingStatus(String processingStatus) {
-		this.processingStatus = processingStatus;
-	}
-
-	public Instant getCreatedAt() {
-		return createdAt;
-	}
-
-	public String getEtag() {
-		return etag;
-	}
-
-	public void setEtag(String etag) {
-		this.etag = etag;
-	}
-
-	public ObjectionLinks getLinks() {
-		return links;
-	}
-
-	public void setLinks(ObjectionLinks links) {
-		this.links = links;
-	}
-
-	public String getKind() {
-		return kind;
-	}
-
-	public void setKind(String kind) {
-		this.kind = kind;
-	}
-
-    public String getPartnerOrganisation() {
-        return partnerOrganisation;
-    }
-
-    public void setPartnerOrganisation(String partnerOrganisation) {
-        this.partnerOrganisation = partnerOrganisation;
-    }
-
-    public Instant getInitialExpirationOn() {
-        return initialExpirationOn;
-    }
-
-    public void setInitialExpirationOn(Instant initialExpirationOn) {
-        this.initialExpirationOn = initialExpirationOn;
-    }
-
-    public String getFailureReason() {
-        return failureReason;
-    }
-
-    public void setFailureReason(String failureReason) {
-        this.failureReason = failureReason;
-    }
-
-    public Instant getProcessingStatusChangedAt() {
-        return processingStatusChangedAt;
-    }
-
-    public void setProcessingStatusChangedAt(Instant processingStatusChangedAt) {
-        this.processingStatusChangedAt = processingStatusChangedAt;
-    }
 }
 
