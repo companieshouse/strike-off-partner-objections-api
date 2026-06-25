c6a7f2835a6fec6016d358c3acea21d96bd7d896
sampankumar <skumar@companieshouse.gov.uk>
2026-06-25 12:48:37 +0100

    Some code changes I had to do to make the service work at my end

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/service/CompanyProfileService.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/service/CompanyProfileService.java
@@ -6,6 +6,7 @@ import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.ApiResponse;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.strikeoffpartnerobjectionsapi.exception.ServiceException;
 
@@ -15,11 +16,10 @@ import static uk.gov.companieshouse.strikeoffpartnerobjectionsapi.utils.Strikeof
 public class CompanyProfileService {
 
     private static final UriTemplate GET_COMPANY_URI = new UriTemplate("/company/{companyNumber}");
-
-    private final InternalApiClient internalApiClient;
-
-    public CompanyProfileService(InternalApiClient internalApiClient) {
-        this.internalApiClient = internalApiClient;
+    private ApiClientService apiClientService;
+    
+    public CompanyProfileService(ApiClientService apiClientService) {
+        this.apiClientService = apiClientService;
     }
 
     public CompanyProfileApi getCompanyProfile(String companyNumber) {
@@ -28,33 +28,49 @@ public class CompanyProfileService {
 
         CompanyProfileApi companyProfileApi;
         String uri = GET_COMPANY_URI.expand(companyNumber).toString();
-        String basePath = internalApiClient.getBasePath();
-        String fullUrl = (basePath == null ? "" : basePath) + uri;
         String chsApiKey = System.getenv("CHS_API_KEY");
         String keyTail = chsApiKey == null || chsApiKey.length() < 4
                 ? "unset"
                 : chsApiKey.substring(chsApiKey.length() - 4);
-        LOGGER.debug("Company profile request method=GET uri=" + uri + " fullUrl=" + fullUrl);
-        LOGGER.debug("Company profile client basePath=" + basePath
-                + ", paymentsBasePath=" + internalApiClient.getBasePaymentsPath()
-                + ", documentBasePath=" + internalApiClient.getBaseDocumentAPIPath()
-                + ", authHeaderConfigured=true"
-                + ", chsApiKeyLength=" + (chsApiKey == null ? 0 : chsApiKey.length())
-                + ", chsApiKeyLast4=" + keyTail);
+//        LOGGER.debug("Company profile request method=GET uri=" + uri + " fullUrl=" + fullUrl);
+//        LOGGER.debug("Company profile client basePath=" + basePath
+//                + ", paymentsBasePath=" + internalApiClient.getBasePaymentsPath()
+//                + ", documentBasePath=" + internalApiClient.getBaseDocumentAPIPath()
+//                + ", authHeaderConfigured=true"
+//                + ", chsApiKeyLength=" + (chsApiKey == null ? 0 : chsApiKey.length())
+//                + ", chsApiKeyLast4=" + keyTail);
+
+//        try {
+//            InternalApiClient internalClient = ApiSdkManager.getPrivateSDK();
+//            companyProfile = apiClientService.getInternalApiClient().company().get(uri).execute().getData();
+//            CompanyProfileApi companyProfileApi1 = internalClient.privateCompanyResourceHandler()
+//                    .getCompanyProfile("/company/" + companyNumber);
+//            ApiResponse<CompanyProfileApi> response = companyProfileApi1.execute();
+//            companyProfileApi = internalApiClient.company().get(uri).execute().getData();
+//            LOGGER.debug("Company profile response status=200 uri=" + uri);
+//        } catch (ApiErrorResponseException e) {
+//            LOGGER.error("Company profile request failed status=" + e.getStatusCode()
+//                    + " reason=" + e.getStatusMessage()
+//                    + " uri=" + uri
+//                    + " response=" + e.getContent());
+//            throw new ServiceException("Error retrieving company profile", e);
+//        } catch (URIValidationException e) {
+//            throw new ServiceException("Invalid URI for company resource", e);
+//        }
+
+        CompanyProfileApi companyProfile;
 
         try {
-            companyProfileApi = internalApiClient.company().get(uri).execute().getData();
-            LOGGER.debug("Company profile response status=200 uri=" + uri);
+            companyProfile = apiClientService.getInternalApiClient().company().get(uri).execute().getData();
         } catch (ApiErrorResponseException e) {
-            LOGGER.error("Company profile request failed status=" + e.getStatusCode()
-                    + " reason=" + e.getStatusMessage()
-                    + " uri=" + uri
-                    + " response=" + e.getContent());
             throw new ServiceException("Error retrieving company profile", e);
         } catch (URIValidationException e) {
+
             throw new ServiceException("Invalid URI for company resource", e);
         }
 
-        return companyProfileApi;
+        return companyProfile;
+
+//        return companyProfileApi;
     }
 }
