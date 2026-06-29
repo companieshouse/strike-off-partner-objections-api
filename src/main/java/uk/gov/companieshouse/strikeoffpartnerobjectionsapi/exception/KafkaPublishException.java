d079de5775b514e5f729471fbf9daed1774200e2
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-06-29 15:43:44 +0100

    TRACS-92 - unused constructor removed

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/exception/KafkaPublishException.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/exception/KafkaPublishException.java
@@ -3,10 +3,6 @@ package uk.gov.companieshouse.strikeoffpartnerobjectionsapi.exception;
 public class KafkaPublishException extends RuntimeException {
     private final String eventId;
 
-    public KafkaPublishException(String message, Throwable cause) {
-        this(message, null, cause);
-    }
-
     public KafkaPublishException(String message, String eventId, Throwable cause) {
         super(message, cause);
         this.eventId = eventId;
