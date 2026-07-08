d46963af817c387b9d4f0ce71c2df7e52a901edf
rmcnally1-ch <rmcnally1@companieshouse.gov.uk>
2026-07-08 13:04:33 +0100

    Initial commit, add docs

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/controller/StrikeOffPartnerWithdrawalsController.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/controller/StrikeOffPartnerWithdrawalsController.java
@@ -8,16 +8,35 @@ import uk.gov.companieshouse.api.objections.model.WithdrawAllObjectionsRequest;
 import uk.gov.companieshouse.api.objections.model.WithdrawAllObjectionsResponse;
 import uk.gov.companieshouse.strikeoffpartnerobjectionsapi.service.StrikeOffPartnerWithdrawalsService;
 
+/**
+ * REST controller for strike off partner withdrawal operations.
+ *
+ * <p>Implements {@link StrikeOffPartnerWithdrawalsInterface} to expose endpoints
+ * for retrieving a specific withdrawal and creating a withdrawal for all objections
+ * against a company.</p>
+ */
 @RestController
 public class StrikeOffPartnerWithdrawalsController implements StrikeOffPartnerWithdrawalsInterface {
 
     private final StrikeOffPartnerWithdrawalsService strikeOffPartnerWithdrawalsService;
 
+    /**
+     * Constructs the controller with the service used to process withdrawal operations.
+     *
+     * @param strikeOffPartnerWithdrawalsService service handling withdrawal retrieval and creation
+     */
     public StrikeOffPartnerWithdrawalsController(
             final StrikeOffPartnerWithdrawalsService strikeOffPartnerWithdrawalsService) {
         this.strikeOffPartnerWithdrawalsService = strikeOffPartnerWithdrawalsService;
     }
 
+    /**
+     * Retrieves a single withdrawal for the provided company and withdrawal identifiers.
+     *
+     * @param companyNumber the company number the withdrawal belongs to
+     * @param withdrawalId the unique withdrawal identifier
+     * @return a response entity containing the matched withdrawal and HTTP 200 status
+     */
     @Override
     public ResponseEntity<WithdrawAllObjectionsResponse> getAllWithdrawals(
             final String companyNumber, final String withdrawalId) {
@@ -26,6 +45,13 @@ public class StrikeOffPartnerWithdrawalsController implements StrikeOffPartnerWi
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * Withdraws all objections for the specified company.
+     *
+     * @param companyNumber the company number to apply the withdrawal against
+     * @param withdrawAllObjectionsRequest request payload describing the withdrawal details
+     * @return a response entity containing the created withdrawal record and HTTP 201 status
+     */
     @Override
     public ResponseEntity<WithdrawAllObjectionsResponse> withdrawAllObjections(
             final String companyNumber,
