1b2fb246e4cbbed5f37f43310927498868a4e83a
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-08-06 10:47:12 +0100

    TRACS-156 - code refactored to return Optional and handle absent rusults explicitly via orElseThrow

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/repository/ObjectionRepository.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsapi/repository/ObjectionRepository.java
@@ -1,10 +1,11 @@
 package uk.gov.companieshouse.strikeoffpartnerobjectionsapi.repository;
 
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import uk.gov.companieshouse.strikeoffpartnerobjectionsapi.model.ObjectionDocument;
 
 public interface ObjectionRepository extends MongoRepository<ObjectionDocument, String> {
-    ObjectionDocument findByCompanyNumberAndObjectionId(String companyNumber, String objectionId);
+    Optional<ObjectionDocument> findByCompanyNumberAndObjectionId(String companyNumber, String objectionId);
 
     boolean existsByCompanyNumberAndPartnerOrganisation(String companyNumber, String partnerOrganisation);
 }
